aa50db267304781a151a4bfb72f967388db4b807
Nausherwan Khiljee <nausherwan.khiljee@gmail.com>
2019-06-02 01:09:33 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,24 @@
+var index = require("./root.js")
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+ console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+ console.log(string.toLowerCase())
+}
+
+
+var string = "hello!"
+function sayHiToGrandma(string) {
+  if(string.toLowerCase() === string) {
+    console.log("I Can\'t Hear You")
+  }
+}
