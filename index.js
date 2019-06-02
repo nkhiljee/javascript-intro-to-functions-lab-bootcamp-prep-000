de3cabe35f2c45672c36c7ecd57d10cbb0a57685
Nausherwan Khiljee <nausherwan.khiljee@gmail.com>
2019-06-02 01:48:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ function sayHiToGrandma(string) {
   else if(string.toUpperCase() === string) {
     return "YES INDEED!"
   }
-  else if(string === "I Love you, Grandma")
-    return "I Love You, Too"
+  else if(string === "I Love you, Grandma"){
+    return "I love you, too"
+}
 }
